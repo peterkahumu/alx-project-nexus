31f6cf432d2cb09fb2be9d11fda7ee343094e1b2
peterkahumu <muhumukip@gmail.com>
2025-07-27 00:37:41 +0300

    chore: update CI workflows and settings for improved environment handling

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,8 @@ env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+env_file = os.getenv("DJANGO_ENV_FILE", ".env")
+environ.Env.read_env(os.path.join(BASE_DIR, env_file))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -86,8 +87,8 @@ DATABASES = {
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST", default="localhost"),
-        "PORT": env("DB_PORT", default="5432"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 

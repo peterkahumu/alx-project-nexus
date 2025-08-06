0c75668e4aeec331ab1a8bef8ae6520902eb3feb
peterkahumu <muhumukip@gmail.com>
2025-08-06 21:33:08 +0300

    feat: enhance admin interface for Cart and CartItem models fix: add verbose name plural for Category model

--- cart/admin.py
+++ cart/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Cart, CartItem
 
+
 # Register your models here.
-admin.site.register(Cart)
-admin.site.register(CartItem)
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    """Manage cart in the admin page"""
+
+    list_display = ["user"]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ["cart", "product", "quantity"]

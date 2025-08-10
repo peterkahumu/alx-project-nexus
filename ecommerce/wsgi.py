ef93fa3fe2075dfd2c821042fee87a1aeaf7eb1d
peterkahumu <muhumukip@gmail.com>
2025-08-10 07:27:46 +0300

    reconfigrure settings 2

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings.py")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
 
 application = get_wsgi_application()
 
